ed2567a7fae67107cbf66f77ebbb51ebbbcf4662
vilerd <30482085+vilerd@users.noreply.github.com>
2020-05-02 15:35:33 +0300

    Update webmaster.sh

--- webmaster.sh
+++ webmaster.sh
@@ -1,42 +1,14 @@
 #!/bin/bash
 
-cat baner
+MYSQL = 1993
+BASENAME = wordpress
+USERNAME = vilerd
+PASSWD = 1993
 
-echo -e "\e[1;36m~~~Please! Read before installing README!~~~\e[0m"
-
-echo -n -e "\e[1;32mEnter SSH port:\e[0m"
-read -s SSH
-echo
-echo -n -e "\e[1;32mEnter password for root user mysql:\e[0m"
-read -s MYSQL
-echo
-echo -n -e "\e[1;32mEnter name wordpress database:\e[0m"
-read -s BASENAME
-echo
-echo -n -e "\e[1;32mEnter name user database:\e[0m"
-read -s USERNAME
-echo
-echo -n -e "\e[1;32mEnter password for database:\e[0m"
-read -s PASSWD
-echo
-
-echo -e "\e[1;34mSSH port ='$SSH'\e[0m"
 echo -e "\e[1;34mMySQL-password ='$MYSQL'\e[0m"
 echo -e "\e[1;34mName wordpress database = '$BASENAME'\e[0m"
 echo -e "\e[1;34mName user database = '$USERNAME'\e[0m"
 echo -e "\e[1;34mPassword database = '$PASSWD'\e[0m"
-
-echo "Are the entered data correct? (y/N) "
-read item
-case "$item" in
-    y|Y) echo -e "\e[1;32mGO!\e[0m"
-        ;;
-    n|N) echo -e "\e[1;31mGoodbye!\e[0m" && exit
-        exit 0
-        ;;
-    *) echo -e "\e[1;31mError!!! Incorrect choice! Try again!\e[0m" && exit
-        ;;
-esac
 
 mkdir /tmp/bootlog
 
@@ -44,63 +16,12 @@ echo -e "\e[1;34mLogging is performed in /tmp/bootlog/logboot.txt\e[0m"
 echo "Start" >> /tmp/bootlog/logboot.txt
 echo -e "\e[1;32mDirectory for logging created!\e[0m"
 
-ADDRESS="google.com.ua"
-if ping -c 1 -s 1 -W 1 $ADDRESS
-then
-echo -e "\e[1;32mConnection OK!\e[0m"
-echo "Network OK!" >> /tmp/bootlog/logboot.txt
-else
-echo -e "\e[1;31mConnection Lost!!!\e[0m"
-echo -e "\e[1;33mCheck network connection!\e[0m"
-echo "Network faill" >> /tmp/bootlog/logboot.txt
-exit
-fi
 
 sudo apt-get update && apt-get upgrade -y
-echo -e "\e[1;32mSystem updated\e[0m"
-echo "System updated" >> /tmp/bootlog/logboot.txt
-
-
-sudo apt-get install ssh -y
-sed -i '/Port 22/c\Port '$SSH'' /etc/ssh/sshd_config
-sed -i '/#ListenAddress 0.0.0.0/c\ListenAddress 0.0.0.0' /etc/ssh/sshd_config
-echo -e "\e[1;32mThe SSH package is installed The connection port is specified\e[0m"
-/etc/init.d/ssh restart
-echo "Install SSH Port=$SSH" >> /tmp/bootlog/logboot.txt
-
-sudo mv demonforiptables /etc/init.d/iptables
-sudo chmod +x /etc/init.d/iptables
-sudo mkdir /etc/iptables.d
-sed -i '/-A PREROUTING -p tcp --dport 22 -j REDIRECT --to-ports 2202/c\-A PREROUTING -p tcp --dport 22 -j REDIRECT --to-ports '$SSH'' iptablesactive
-sleep 1s
-sudo  mv iptablesactive /etc/iptables.d/active
-sleep 1s
-sudo mv iptablesinactive /etc/iptables.d/inactive
-sleep 1s
-/etc/init.d/iptables start
-iptables-save
-echo "#! /sbin/iptables-restore" > /etc/network/if-up.d/iptables-rules
-iptables-save >> /etc/network/if-up.d/iptables-rules
-sudo chmod +x /etc/network/if-up.d/iptables-rules
-sudo ls -lA /etc/network/if-up.d/ipt*
-echo -e "\e[1;32mIPtables active\e[0m"
-echo "Iptables active!" >> /tmp/bootlog/logboot.txt
-
-sudo apt-get install fail2ban -y
-sed -i '/port     = 2202/c\port     = '$SSH'' /tmp/web_master/fail2ban2
-sed -i '/action   = iptables/c\action   = iptables[name=SSH, port='$SSH', protocol=tcp]' /tmp/web_master/fail2ban2
-mv fail2ban2 /etc/fail2ban/jail.local
-/etc/init.d/fail2ban restart
-echo -e "\e[1;32mFail2ban istall\e[0m"
-echo "Fail2ban install | Fail2ban active!" >> /tmp/bootlog/logboot.txt
 
 sudo apt-get install vim -y
 echo -e "\e[1;32mInstall vim\e[0m"
 echo "Install vim" >> /tmp/bootlog/logboot.txt
-
-sudo apt-get install mc -y
-echo -e "\e[1;32mInstall mc\e[0m"
-echo "Install mc" >> /tmp/bootlog/logboot.txt
 
 sudo apt-get install ntp -y
 echo -e "\e[1;32mInstall ntp\e[0m"
@@ -182,13 +103,6 @@ rm -r /tmp/boot
 echo -e "\e[1;31mGarage remove\e[0m"
 echo "Garbage remove" >> /tmp/bootlog/logboot.txt
 
-echo -e "\e[1;34mSSH port ='$SSH'\e[0m"
-echo -e "\e[1;34mMySQL-password ='$MYSQL'\e[0m"
-echo -e "\e[1;34mName wordpress database = '$BASENAME'\e[0m"
-echo -e "\e[1;34mName user database = '$USERNAME'\e[0m"
-echo -e "\e[1;34mPassword database = '$PASSWD'\e[0m"
-
-echo "SSH port = $SSH" >> /tmp/bootlog/logboot.txt
 echo "MySQL-pass = $MYSQL" >> /tmp/bootlog/logboot.txt
 echo "Name wordpress database = $BASENAME" >> /tmp/bootlog/logboot.txt
 echo "Name user database = $USERNAME" >> /tmp/bootlog/logboot.txt
